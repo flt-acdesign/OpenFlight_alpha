8e0958f37a755dd860643bee57ece5e2eecf6b3c
r-acad <81679447+r-acad@users.noreply.github.com>
2024-12-21 15:41:48 +0100

    name changes for clarity

--- src/JAVASCRIPT/60_MAIN_scene_and_render_engine/MAIN_render_loop.js
+++ src/JAVASCRIPT/60_MAIN_scene_and_render_engine/MAIN_render_loop.js
@@ -24,24 +24,19 @@ window.addEventListener("DOMContentLoaded", function () {
         // Update forces and moments based on joystick input
         updateForcesFromJoystickOrKeyboard(scene)
 
-        if (aircraft !== undefined) {
-          sendStateToServer(global_time_step)
-        }
+        if (aircraft !== undefined) { sendStateToServer(global_time_step) } // Send state to server if aircraft is defined
       }
     }
 
-    // Update displayed coordinates and speed
-    if (aircraft !== undefined) {
-      updateInfo()
-    }
+    // Update displayed coordinates and speed once the aircraft is defined (active object)
+    if (aircraft !== undefined) { updateInfo() }
 
-    scene.render()
+    scene.render() // Render the scene
+
   })
 
   // Resize the engine on resize
-  window.addEventListener("resize", function () {
-    engine.resize()
-  })
+  window.addEventListener("resize", function () {  engine.resize()  }) // Resize the engine when the window is resized
 
   // Gamepad event listeners
   window.addEventListener("gamepadconnected", (event) => {
@@ -49,8 +44,10 @@ window.addEventListener("DOMContentLoaded", function () {
     console.log(`Gamepad connected at index ${gamepadIndex}: ${event.gamepad.id}.`)
   })
 
-  window.addEventListener("gamepaddisconnected", () => {
+  window.addEventListener("gamepaddisconnected", () => { // Gamepad disconnected
     console.log("Gamepad disconnected.")
     gamepadIndex = null
   })
-})
+
+}) // End of window.addEventListener("DOMContentLoaded", function () { ... })
+
